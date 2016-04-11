6b54d163c4a34e001d99940462acb63d9e014704
ankit agarwal <ankit.agarwal02@snapdeal.com>
2016-04-11 11:09:54 +0530

    Deleted registration. ONLY LOGIN

--- SplitCash/src/com/dao/RegistrationDaoI.java
+++
@@ -1,7 +0,0 @@
-package com.dao;
-
-public interface RegistrationDaoI {
-	
-	public int addUser(int user_id, String username, String first_name, String last_name,String email, String password);
-	public int maxKey(String table_name, String id);
-}
